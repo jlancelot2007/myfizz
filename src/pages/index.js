b8a5d5a47eeea6f86182750100c5baeb51311014
Andrew Plater <aplater@myfizz.biz>
2018-10-04 08:31:16 +1000

    updated Sunny photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,12 +20,12 @@ import full05 from '../assets/images/fulls/05.jpg'
 import full06 from '../assets/images/fulls/06.jpg'
 
 const DEFAULT_IMAGES = [
-    { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'},
-    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.'}
+    { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Sunny loves to dress up.'},
+    { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'She is also always eager to please.'},
+    { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Her pearl necklace sets her as Queen of Marrickville.'},
+    { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'She always has a toothy grin.'},
+    { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Ever eager for more attention.'},
+    { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Sunny loves to pose for the camera.'}
 ];
 
 class HomeIndex extends React.Component {
